eaa3da2ff6c7c8b209f6c10bc102d9f2c5a5279f
Viktoria <vik25@i.ua>
2023-05-05 19:27:17 +0300

    remove fetchUsers

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+const page = 1;
+
 const URL = "https://642fd67cb289b1dec4bb2a03.mockapi.io/users";
+const params = {
+  completed: false,
+  page: page,
+  limit: 5,
+};
 
 export const fetchUsers = createAsyncThunk(
   "users/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get(URL);
+      const { data } = await axios.get(URL, { params });
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
